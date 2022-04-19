abdb0bb7d2941747946913fb099d16d59a6921c6
phongvh <phongvh1405@gmail.com>
2022-04-19 10:03:44 +0700

    #update: script.js

--- day9-sound-board/script.js
+++ day9-sound-board/script.js
@@ -1,25 +1,25 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+sounds.forEach((sound) => {
+  const button = document.createElement("button");
+  button.innerText = sound;
 
-    btn.innerText = sound
+  button.classList.add('btn')
 
-    btn.addEventListener('click', () => {
-        stopSongs()
+  button.addEventListener('click', () => {
+    stopSongs()
+    
+    document.getElementById(sound).play()
+  })
 
-        document.getElementById(sound).play()
-    })
-
-    document.getElementById('buttons').appendChild(btn)
-})
+  document.getElementById("buttons").appendChild(button);
+});
 
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.pause()
-        song.currentTime = 0;
-    })
+    song.pause();
+    song.currentTime = 0;
+  });
 }
